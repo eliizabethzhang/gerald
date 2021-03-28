3bb925efca6841bbdde7b77eb75c2faa9c1438cb
NotPaingu <pa1ngubusiness@gmail.com>
2021-03-28 10:45:53 -0400

    added welcome and farewell logging

--- bot.py
+++ bot.py
@@ -3,12 +3,20 @@ from discord.ext import commands
 from dotenv import load_dotenv
 import os
 
-client = commands.Bot(command_prefix= '.')
+client = commands.Bot(command_prefix='.')
+
 
 @client.event
 async def on_ready():
     print('Bot is ready')
 
-#Credentials
+@client.event
+async def on_member_join(member):
+    print(f'{member} has joined the server.')
+
+async def on_member_remove(member):
+    print(f'{member} has left the server')
+
+# Credentials
 load_dotenv('.env')
 client.run(os.getenv('BOT_TOKEN'))
