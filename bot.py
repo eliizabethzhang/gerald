f1cf749bb4332c42384f7cd2acc0bb1067d4591b
NotPaingu <pa1ngubusiness@gmail.com>
2021-03-28 13:30:14 -0400

    Did a rough patchwork because we didn't have enough time to actually add functionality

--- bot.py
+++ bot.py
@@ -1,17 +1,15 @@
 import discord
 from discord.ext import commands
+from discord.ext.commands import MissingRequiredArgument
 from dotenv import load_dotenv
 import os
 import pymongo
 from pymongo import MongoClient
-
-
-
+import random
 
 client = commands.Bot(command_prefix='.')
 intents = discord.Intents().all()
 client = commands.Bot(command_prefix='.', intents=intents)
-
 
 
 @client.event
@@ -28,16 +26,35 @@ async def on_member_join(member):
 async def on_member_remove(member):
     print(f'{member} has left the server')
 
+
 @client.command()
 async def ping(ctx):
-    await ctx.send(f'Pong! {round(client.latency*1000)} ms')
+    await ctx.send(f'Pong! {round(client.latency * 1000)} ms')
 
 
+@client.command(aliases=['ccreate'])
+async def classescreate(ctx, *, rest):
+    await ctx.send(f"Created the class {rest}")
 
-cluster = MongoClient(os.getenv('MONGO_URL'))
-db = cluster["UserData"]
-collection = db["UserData"]
+@client.command(aliases=['cdelete'])
+async def classesdelete(ctx, *, rest):
+    await ctx.send(f"Deleted the class {rest}")
 
+@client.command(aliases=['tadd'])
+async def taskadd(ctx, classnum, *, rest):
+    await ctx.send(f'added the task {rest} to class number {classnum}')
+
+@client.command(aliases=['tdel'])
+async def taskdelete(ctx, classnum, *, rest):
+    await ctx.send(f'deleted the task {rest} from class number {classnum}')
+
+@client.command(aliases=['tlist'])
+async def tasklist(ctx, classnum):
+    await ctx.send(f'Your tasks for class {classnum} are: \nAction Plan Deadline: 3/31/2021\nEssay/Discussion Deadline: 4/1/2021')
+
+@client.command(aliases=['clist'])
+async def classeslist(ctx):
+    await ctx.send(f'Your classes are:\n CLyerly 2A Differential Equations\n COgren 4B APES')
 # Credentials
 load_dotenv('.env')
 client.run(os.getenv('BOT_TOKEN'))
